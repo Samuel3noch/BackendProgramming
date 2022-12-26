52aa4b4ee6e1db8f85c3a373dd53e6691389eaef
Samuel/Sem <114405673+Samuel3noch@users.noreply.github.com>
2022-12-26 17:01:01 +0100

    Update Datatypen.php

--- 1.4 Datatypen/Datatypen.php
+++ 1.4 Datatypen/Datatypen.php
@@ -27,6 +27,7 @@ $koers = 1.2;
 $euros = $dollars * $koers;
 
 // ik ga de variabellen nu berekenen op 2 decimalen.
+
 //hier beneden heb ik almijn tekst in een "Heredoc-syntaxis" gedaan.
 echo <<<SOM
 <br>
