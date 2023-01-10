9ab8904dfa56bddeae63444b0cf018276880e713
Samuel3noch <1029344@mborijnland.nl>
2023-01-10 22:50:40 +0100

    Main

---
+++ 1.5 Array-methodes/arrays.php
@@ -0,0 +1,16 @@
+<?php 
+// ik heb losse arrays toegevoegd aan de opdracht.
+echo "<br>---- Opgave 5"; 
+$producten = [];
+$producten[0] = "Boeken";
+$producten[1] = "CD's";
+$producten[2] = "Smartphones";
+$producten[3] = "DVD's";
+
+//met deze methode print ik snel de inhoud van een array uit.
+echo "<br>---- Opgave 6: print_r()";
+print_r($producten);
+
+
+
+?>
